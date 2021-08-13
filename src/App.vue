e5fbcf6f0dd3504860a3526dbca5ac33fd69de53
ProDeveloper1996 <ProDev1996@gmail.com>
2021-08-12 21:16:38 -0700

    vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MyfirstVue msg="My First Vue Project" />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import MyfirstVue from './components/myFirstVue.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyfirstVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/myFirstVue.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app_1">
+        {{msg}}
+        VUE
+        <!-- <button v-on: click="reverseMessage">ReverseMessage</button> -->
+    </div>
+</template>
+<script>
+// var app1 = new vue({
+//     el: "#app_1",
+//     data: {
+//         message: 'My First Vue'
+//     },
+//     methods: {
+//         reverseMessage: function() {
+//             this.message = this.message.split('').reverse('').join('')
+//         }
+//     }
+// })
+export default {
+    name: 'MyfirstVue',
+    // app1,
+    props: {
+        msg: Object
+    }
+}
+</script>
